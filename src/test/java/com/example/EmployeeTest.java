2fff51679531cb00b67c0538bde8362a9555444d
Oliver Säfström <safstrom.oliver@gmail.com>
2023-01-18 15:57:50 +0100

    add test methods for EmployeeTest.java

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -7,9 +7,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmployeeTest {
 	Employee employee = new Employee("1", 123);
 	@Test
-	void createEmployee() {
-		assertEquals("1", employee.getId());
-		assertEquals(123, employee.getSalary());
+	void employeeToString() {
+		assertEquals("Employee [id=1, salary=123.0]", employee.toString());
+
+	}
+	@Test
+	void isNotPaid() {
+		assertFalse(employee.isPaid());
+
+	}
+
+	@Test
+	void isPaid() {
+		employee.setPaid(true);
+		assertTrue(employee.isPaid());
+
+	}
+
+	@Test
+	void changeEmployeeId() {
+		employee.setId("2");
+		assertEquals("2", employee.getId());
+	}
+
+	@Test
+	void changeEmployeeSalary() {
+		employee.setSalary(1234);
+		assertEquals(1234, employee.getSalary());
+
 	}
 
 
