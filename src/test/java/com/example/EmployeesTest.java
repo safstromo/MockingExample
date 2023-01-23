230c17f4430d590b2bf766695d6e49895aaf6215
Oliver Säfström <safstrom.oliver@gmail.com>
2023-01-23 09:28:43 +0100

    Code cleanup

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,29 +1,28 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
 
 class EmployeesTest {
 	EmployeeRepositoryImpl employeeRepository = new EmployeeRepositoryImpl(List.of
 			(new Employee("1", 123), new Employee("2", 12345)));
 	BankService bankService = Mockito.mock(BankService.class);
+	Employees employees = new Employees(employeeRepository, bankService);
 
 
 	@Test
 	void payEmployeesPaymentIncrease() {
-		Employees employees = new Employees(employeeRepository, bankService);
 		assertEquals(2, employees.payEmployees());
-
 	}
 
 	@Test
 	void employeePaid() {
-		Employees employees = new Employees(employeeRepository, bankService);
 		employees.payEmployees();
 
 		assertTrue(employeeRepository.findAll().get(0).isPaid());
@@ -31,8 +30,7 @@ class EmployeesTest {
 
 	@Test
 	void payEmployeeNotPaid() {
-		Employees employees = new Employees(employeeRepository, bankService);
-		doThrow(RuntimeException.class).when(bankService).pay("1",123);
+		doThrow(RuntimeException.class).when(bankService).pay("1", 123);
 		employees.payEmployees();
 
 		assertFalse(employeeRepository.findAll().get(0).isPaid());
